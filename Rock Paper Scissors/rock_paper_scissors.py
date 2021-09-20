3e4a123b284823ea881cb63c0812c056d0b480b4
adnanekouna-a <adnanekouna.a@gmail.com>
2021-09-20 19:23:15 +0100

    Add Number Guessing Game

--- Rock Paper Scissors/rock_paper_scissors.py
+++ Rock Paper Scissors/rock_paper_scissors.py
@@ -2,12 +2,11 @@ import random
 
 possible_choices = ['Rock', 'Paper', 'Scissors']
 choices_input = {'R': 'Rock',  'P': 'Paper', 'S': 'Scissors'}
-result = ''
 
 print('Welcome to Rock. Paper. Scissors !')
 print('Enter your decision ! [R : Rock, P : Paper, S : Scissors]')
 
-player_choice = choices_input[input('> ')]
+player_choice = choices_input[input('> ').upper()]
 cpu_choice = random.choice(possible_choices)
 
 print(f'----> Player chooses {player_choice}')
